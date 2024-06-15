304a81c8c68260504ec62d172e1ca3871c7ddbb3
chenos <chenlinxh@gmail.com>
2024-06-15 11:26:36 +0800

    chore: update Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:alpine
+FROM node:18.20.3-slim as builder
 
 ENV SITE_APPDIR /site
 ENV SITE_PORT 4321
@@ -10,11 +10,19 @@ COPY ./src $SITE_APPDIR/src
 COPY ./astro.config.mjs $SITE_APPDIR/astro.config.mjs
 COPY ./tsconfig.json $SITE_APPDIR/tsconfig.json
 COPY ./package.json $SITE_APPDIR/package.json
-
+COPY ./pkg.mjs $SITE_APPDIR/pkg.mjs
 RUN cd $SITE_APPDIR && yarn install && yarn astro build
+RUN cd $SITE_APPDIR && node pkg.mjs
+RUN cd $SITE_APPDIR && rm -rf node_modules
+RUN cd $SITE_APPDIR && yarn install --production
 
+FROM node:18.20.3-slim
+
+ENV SITE_APPDIR /site
+ENV SITE_PORT 4321
+
+COPY --from=builder /site /site
 EXPOSE $SITE_PORT
-
 WORKDIR $SITE_APPDIR
 
 CMD ["node", "./dist/server/entry.mjs"]
---
+++ pkg.mjs
@@ -0,0 +1,36 @@
+import fs from 'fs/promises';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const pkgs = [
+    'kleur',
+    'clsx',
+    '@astrojs/internal-helpers',
+    'dayjs',
+    '@astrojs/markdown-remark',
+    'rehype-slug',
+    'remark-directive',
+    'mrmime',
+    '@astrojs/rss',
+    'p-limit',
+    'send',
+    'server-destroy',
+    'cookie',
+    'path-to-regexp',
+    'html-escaper',
+    'cssesc'
+];
+
+(async () => {
+    const pkg = await fs.readFile(resolve(__dirname, 'package.json'), 'utf-8');
+    const pkgJson = JSON.parse(pkg);
+    pkgJson.dependencies = {};
+    for (const pkg of pkgs) {
+        const content = await fs.readFile(resolve(__dirname, 'node_modules', pkg, 'package.json'), 'utf-8');
+        const json =  JSON.parse(content.toString());
+        pkgJson.dependencies[pkg] = json.version;
+    }
+    await fs.writeFile(resolve(__dirname, 'package.json'), JSON.stringify(pkgJson, null, 2));
+})()
